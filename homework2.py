1df1d29b193379d595b705c8bff8cce45226cc72
Zahar25 <zpyshny@gmail.com>
2021-11-29 14:39:57 +0200

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,8 +1,7 @@
-def bank(a, years, percents):
+def bank(a, years, percents= 10):
     for year in range(years):
         a = a*(percents/100)+a 
     return "%.2f" % a
 a = int(input("Enter sum of vklad: "))
 years = int(input("Enter years: "))
-percents = int(input('Enter percent: '))
-print(bank(a, years, percents))
+print(bank(a, years))
